c0509682526d0ab313eb11cf405fe06858a46c04
Tazim <tazim0877@gmail.com>
2024-01-11 17:19:43 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className=" text-4xl text-center">Weather Application v1.0</h2>
+      <h2 className="text-xl text-center">Working on it...</h2>
+    </>
+  );
+}
+
+export default App;
